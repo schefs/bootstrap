bdbae8bd0d8505a70fa93cfa571571c989fd489a
unknown <eyalschefler@gmail.com>
2019-01-17 14:18:26 +0200

    added ingress controller to deploy script

--- manifests/deploy.sh
+++ manifests/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#Deploy kube-promethues: grafana, prometheus operator, alert manager cluster
+# Deploy ingress controller
+
+kubectl apply -f haproxy-ingress.yaml
+
+# Deploy kube-promethues: grafana, prometheus operator, alert manager cluster
 
 kubectl create -f kube-prometheus/ || true
 # It can take a few seconds for the above 'create manifests' command to fully create the following resources, so verify the resources are ready before proceeding.
@@ -9,3 +13,5 @@ until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; 
 
 kubectl create -f kube-prometheus/ 2>/dev/null || true  # This command sometimes may need to be done twice (to workaround a race condition).
 
+
+#
