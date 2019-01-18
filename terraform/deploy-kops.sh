95ee132d8ca3b9e5eefde610ef1a9e7f717a609f
unknown <eyalschefler@gmail.com>
2019-01-18 23:05:53 +0200

    created deploy kops script

---
+++ terraform/deploy-kops.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export KOPS_STATE_STORE=s3://$(terraform output kops_s3_state_bucket)
+export KOPS_CLUSTER_NAME=$(terraform output dns_zone_name)
+
+kops create cluster \
+     --cloud=aws \
+     --state=$KOPS_STATE_STORE \
+     --node-count 3 \
+     --zones $(terraform.exe output AZ |tr -d '\n') \
+     --master-zones $(terraform.exe output AZ |tr -d '\n') \
+     --dns-zone=$(terraform output dns_zone_id) \
+     --vpc=$(terraform output vpc_id) \
+     --dns private \
+     --node-size t2.small \
+     --master-size t2.micro \
+     --topology private \
+     --networking calico \
+     --bastion \
+     --ssh-public-key ~/.ssh/id_rsa.pub \
+     --cloud-labels $(terraform output common_tags|tr '\n' ','|sed 's/ //g'|sed 's/.$//') \
+     --name $KOPS_CLUSTER_NAME
+
