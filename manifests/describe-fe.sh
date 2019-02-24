734ba74d5dc7dc9cc52dc51dce7bc29f618965f3
unknown <eyalschefler@gmail.com>
2019-02-24 21:07:57 +0200

    changed ui service type and dc name for consul

--- manifests/describe-fe.sh
+++ manifests/describe-fe.sh
@@ -18,3 +18,10 @@ echo -e "\e[0mKibana UI is availabe in:\n\e[32m$SERVER$KIBANA_PATH"
 # Dashboard access
 DASHBOARD_PATH=/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
 echo -e "\e[0mK8s Dashboard UI is availabe in:\n\e[32m$SERVER$DASHBOARD_PATH"
+
+# Consul UI 
+ CONSUL_SVC_NAME =  $(kubectl get svc -n consul -o NAME | grep consul-ui)
+ CONSUL=$(kubectl get svc -n consul $CONSUL_SVC_NAME -o jsonpath='{.status.loadBalancer.ingress[?(@.hostname)].*}')
+ echo -e "\e[0mK8s Consul UI is availabe in:\n\e[32mhttp://$consul"
+
+
